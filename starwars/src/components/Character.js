10931eedc80d99846330e1f0d4c58289a37fa63b
max-keller427 <max.keller427@gmail.com>
2019-03-09 11:49:44 -0700

    got the list on the screen

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Character(props) {
+  
+  return (
+    <div className="character">
+      <h3>Name: {props.character.name}</h3>
+      <ul>
+        <p>Birth Year: {props.character.birth_year}</p>
+        <p>Created: {props.character.created}</p>
+        <p>Edited: {props.character.edited}</p>
+        <p>Eye Color: {props.character.eye_color}</p>
+        <p>Films: for (let i = 0; i < props.character.films; i++) {
+    <li>{props.character.films}</li>
+    
+}</p>
+        <p>Gender: {props.character.gender}</p>
+        <p>Hair Color: {props.character.hair_color}</p>
+        <p>Height: {props.character.height}</p>
+        <p>Homeworld: {props.character.homeworld}</p>
+        <p>Mass: {props.character.mass}</p>
+        <p>Skin Color: {props.character.skin_color}</p>
+        <p>Species: {props.character.species}</p>
+        <p>Starships: {props.character.starships}</p>
+        <p>URL: {props.character.url}</p>
+        <p>Vehicles: {props.character.vehicles}</p>
+      </ul>
+    </div>
+  );
+}
+
+export default Character;
+
+// for (let i = 0; i < [props.character.films]; i++) {
+//     <li>props.character.films</li>
+    
+// }
